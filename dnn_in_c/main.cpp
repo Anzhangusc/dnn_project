cfe7ab34828ee8f19153567d796ebe72ea59e8de
anzhang <anzhang@usc.edu>
2018-01-25 19:48:07 -0800

    Initial Commit

---
+++ dnn_in_c/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  dnn_in_c
+//
+//  Created by 张安 on 1/25/18.
+//  Copyright © 2018 anzhang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
